556c1f01e7bda7d4b8f92828360e5d206783f080
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-07-18 07:56:29 +0900

    Vue.jsを使用する環境設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+const { VueLoaderPlugin } = require('vue-loader')
+environment.plugins.prepend(
+  'VueLoaderPlugin',
+  new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue', {
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
+})
+
 module.exports = environment
