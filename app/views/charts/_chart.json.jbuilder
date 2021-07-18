4b1b4c966623703f26d0b2c98ddf8b894764fc2b
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-07-19 08:46:07 +0900

    chartモデルをscaffoldで作成

---
+++ app/views/charts/_chart.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! chart, :id, :title, :chart_code, :created_at, :updated_at
+json.url chart_url(chart, format: :json)
---
+++ db/migrate/20210718233920_create_charts.rb
@@ -0,0 +1,10 @@
+class CreateCharts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :charts do |t|
+      t.string :title
+      t.text :chart_code
+
+      t.timestamps
+    end
+  end
+end
