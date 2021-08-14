d0af2f85711b55b39088d6b21268de0896df2ba3
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-08-14 11:32:47 +0900

    users#showページを追加
    サインアップ後、ログイン後、ログアウト後、プロフィール編集後のリダイレクト先を設定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,14 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    users_show_path(resource)
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'charts#index'
+  get 'users/show'
   devise_for :users, controllers: {
+    registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   resources :charts, except: %i(edit)
