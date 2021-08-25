b4a6c590e25a9f84c68dd5848bd60cf9b19a937c
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-08-25 13:35:45 +0900

    deviseのviewにスタイルを当てた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
