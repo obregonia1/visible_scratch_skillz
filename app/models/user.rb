e7d555b2f7beb3d095d87d342671dd636667b582
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-08-26 12:17:29 +0900

    twitter認証のユーザー編集時にパスワード入力欄を非表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
 
   has_many :charts, dependent: :destroy
 
+  def no_password
+    !!provider
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email || User.dummy_email(auth)
