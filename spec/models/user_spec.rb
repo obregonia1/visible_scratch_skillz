9398d9fb605fe09005e6dd8de108f86f14fbb124
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-09-10 17:49:47 +0900

    userのモデルテストを追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'メールアドレス、パスワードが有効である' do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'メールアドレスが無い場合無効である' do
+    user = FactoryBot.build(:user, email: '')
+    expect(user).to_not be_valid
+  end
+
+  it 'メールアドレスが重複する場合は無効である' do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user, email: user1.email)
+    expect(user2).to_not be_valid
+  end
+
+  it 'ユーザー名がない場合でも有効である' do
+    user = FactoryBot.build(:user, username: nil)
+    expect(user).to be_valid
+  end
+end
