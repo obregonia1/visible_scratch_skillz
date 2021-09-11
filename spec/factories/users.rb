78e78b816e60b2b6ff8440b6027c1325d76b8507
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-09-11 17:42:28 +0900

    userのシステムテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "vss#{n}@example.com"}
-    password {"password"}
-    username {"Alice"}
+    sequence(:email) { |n| "vss#{n}@example.com" }
+    password { "password" }
+    username { "Alice" }
   end
 end
