7c048e528d8aa514cee0444ddea1f34d89412f00
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-09-13 14:35:20 +0900

    システムスペックをヘッドレスで実行するよう設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   # config.include FactoryBot::Syntax::Methods
   config.include OmniauthMocks
+
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    end
+  end
 end
