8c9b0bb46ace794e4ddbe191df99ebaed6c5d8bd
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-09-15 11:46:02 +0900

    seedデータを作成

--- spec/factories/charts.rb
+++ spec/factories/charts.rb
@@ -3,7 +3,18 @@
 FactoryBot.define do
   factory :chart do
     title { 'Baby' }
-    chart_code { '[{"trick":"baby","pattern":"forward","beatLength":6,"beatPosition":0}]' }
+    chart_code {
+      <<~CODE
+        [{"trick":"baby","pattern":"forward","beatLength":6,"beatPosition":0},
+        {"trick":"baby","pattern":"backward","beatLength":6,"beatPosition":6},
+        {"trick":"baby","pattern":"forward","beatLength":6,"beatPosition":12},
+        {"trick":"baby","pattern":"backward","beatLength":6,"beatPosition":18}]
+      CODE
+    }
+    after(:build) do |chart|
+      title_image = chart.title.downcase.sub /_\d/, ''
+      chart.image.attach(io: File.open("spec/fixtures/files/#{title_image}.png"), filename: "#{title_image}.png", content_type: 'image/png')
+    end
     association :user
   end
 end
