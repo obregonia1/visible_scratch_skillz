24edf93741b7b25db1206e4808ab77fafb6f3236
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-09-16 12:49:57 +0900

    サービス概要ページを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     if user_signed_in?
       redirect_to current_user
     else
-      redirect_to new_user_session_path
+      redirect_to welcome_path
     end
   end
 
